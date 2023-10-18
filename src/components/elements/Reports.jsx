3f06795b65f8ce36ea59264afdfcfdbe343a3f72
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-10-18 19:19:02 +0530

    added the sorting feature

--- src/components/elements/Reports.jsx
+++ src/components/elements/Reports.jsx
@@ -3,12 +3,13 @@ import reportContext from '@/context/report/reportContext'
 import { jost } from '@/utils/fonts'
 import Display from './Display'
 import { formatDateTime } from '@/utils/commonFunctions'
-import { FaTrashAlt } from 'react-icons/fa'
+import { FaFilter, FaTrashAlt } from 'react-icons/fa'
 import { AiTwotoneEdit } from 'react-icons/ai'
 
 const Reports = () => {
     const { getReports, reportData, deleteReport } = useContext(reportContext)
     const [displayedReports, setDisplayedReports] = useState({});
+    const [sortedReports, setSortedReports] = useState([]);
     const [edit, setEdit] = useState(false)
     useEffect(() => {
         getReports()
@@ -17,6 +18,10 @@ const Reports = () => {
     useEffect(() => {
         console.log(reportData)
     }, [reportData])
+
+    useEffect(() => {
+        setSortedReports([...reportData]);
+    }, [reportData]);
 
     const toggleDisplay = (reportId) => {
         setDisplayedReports((prevState) => ({
@@ -30,16 +35,39 @@ const Reports = () => {
         toggleDisplay(reportId)
     }
 
+    // Function to sort by newest first
+    const sortNewestFirst = () => {
+        setSortedReports([...sortedReports].sort((a, b) => {
+            return new Date(b.dateTime) - new Date(a.dateTime);
+        }));
+    };
+
+    // Function to sort by oldest first
+    const sortOldestFirst = () => {
+        setSortedReports([...sortedReports].sort((a, b) => {
+            return new Date(a.dateTime) - new Date(b.dateTime);
+        }));
+    };
+
 
     return (
         <div className={`mainWrapper relative overflow-x-hidden min-h-screen w-full ${jost.className} ${reportData.length != 0 ? 'pt-16' : ''} px-2 lg:px-20`}>
             {reportData.length != 0 ?
                 <>
-                    <div className="heading">
+                    <div className="heading flex items-center justify-between px-2">
                         <h1 className='text-5xl lg:text-6xl font-bold py-10'>All Reports</h1>
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0}>
+                                <FaFilter className='text-2xl' />
+                            </label>
+                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a onClick={sortNewestFirst}>Newest first</a></li>
+                                <li><a onClick={sortOldestFirst}>Oldest first</a></li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="reports flex flex-col lg:flex-row space-y-4 lg:space-x-4 lg:space-y-0">
-                        {reportData.map((report, index) => {
+                        {sortedReports.map((report, index) => {
                             const isDisplayed = !!displayedReports[report.$id];
 
                             return (
